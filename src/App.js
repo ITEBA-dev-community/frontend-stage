1a536ad55d3cc41c4c12d84ffc305389e620417f
AndyNotfound <Andy.Notfound@gmail.com>
2022-07-19 22:48:15 +0700

    react router setting for home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
   );
 }
 
