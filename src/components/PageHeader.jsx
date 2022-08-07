1ddcc792c5be8e966e4067fe5f84e509826c5ffd
AndyNotfound <AndyNotfound@gmail.com>
2022-08-07 20:40:16 +0700

    Account page

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function PageHeader({previousUrl, label}) {
   return (
-    <div className='flex justify-center items-center w-full h-fit pt-5 pb-7 shadow shadow-slate-300 relative mix-blend-normal'>
+    <div className='flex justify-center items-center w-full h-fit pt-5 pb-7 shadow shadow-slate-300 relative mix-blend-normal bg-white'>
         <Link to={previousUrl}>
             <img src='/assets/images/single-back-arrow-icon.svg' className='absolute left-8 bottom-8' />
         </Link>
